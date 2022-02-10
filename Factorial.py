62151ab5106f89c3e4eb0d8567d8a55b9a125bb1
suravichandran <suravichandran@paypal.com>
2022-02-10 12:14:01 +0530

    factorial change

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,8 @@
 def Factorial(num):
-    if num == 1:
+    if num == 1 or num==0:
         return 1
+    if num <0:
+        return None
     return num*Factorial(num-1)
 
-print(Factorial(4))
+print(Factorial(-1))
