79336236b19dfaeb33be0819dcba4e8ad18eecab
suravichandran <suravichandran@paypal.com>
2022-02-13 20:15:39 +0530

    queue modified

--- Stacks.py
+++ Stacks.py
@@ -1,3 +1,5 @@
+'Stack is prepend and pop_first'
+'push pop and print_stack'
 class Node:
     def __init__(self, value):
         self.value=value
@@ -6,34 +8,43 @@ class Stack:
     def __init__(self, value):
         new_node=Node(value)
         self.top=new_node
-        self.height=1
-    def print_stack(self):
-        temp=self.top
-        while temp:
-            print(temp.value)
-            temp=temp.next
-        print('height of the stack is: ',self.height)
+        self.bottom=new_node
+        self.length=1
     def push(self, value):
         new_node=Node(value)
-        if self.height==0:
+        if self.length==0:
             self.top=new_node
+            self.bottom=new_node
         else:
             new_node.next=self.top
             self.top=new_node
-        self.height+=1
+        self.length+=1
         return True
     def pop(self):
-        if self.height==0:
+        if self.length==0:
             return False
         temp=self.top
-        self.top=temp.next
-        temp.next=None
-        self.height-=1
+        if self.length==1:
+            self.top=None
+            self.bottom=None
+        else:
+            temp=self.top
+            after=temp.next
+            temp.next=None
+            self.top=after
+        self.length-=1
         return temp
+    def print_stack(self):
+        temp=self.top
+        for _ in range(self.length):
+            print(temp.value)
+            temp=temp.next      
 
 s=Stack(4)
 s.push(3)
 s.push(5)
+s.pop()
+s.push(6)
 s.pop()
 s.print_stack()
 
