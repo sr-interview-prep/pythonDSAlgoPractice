07ebfe407a568ece7f962cca049280851cdc70a8
suravichandran <suravichandran@paypal.com>
2022-02-13 20:03:44 +0530

    queue

--- Queues.py
+++ Queues.py
@@ -1,3 +1,5 @@
+'queue is append and pop_first'
+'enqueue, dequeue,print_queue'
 class Node:
     def __init__(self, value):
         self.value=value
@@ -8,21 +10,14 @@ class Queue:
         self.first=new_node
         self.last=new_node
         self.length=1
-    def print_queue(self):
-        temp =self.first
-        while temp:
-            print(temp.value)
-            temp=temp.next
-        print('length of the queue is :',self.length)
     def enqueue(self, value):
         new_node=Node(value)
         if self.length==0:
             self.first=new_node
             self.last=new_node
-            return True
-        temp = self.last
-        temp.next=new_node
-        self.last=new_node
+        else:
+            self.last.next=new_node
+            self.last=new_node
         self.length+=1
         return True
     def dequeue(self):
@@ -32,17 +27,24 @@ class Queue:
         if self.length==1:
             self.first=None
             self.last=None
-            return temp
         else:
-            self.first=temp.next
+            after=temp.next
             temp.next=None
+            self.first=after
         self.length-=1
         return temp
-
+    def print_queue(self):
+        temp=self.first
+        for _ in range(self.length):
+            print(temp.value)
+            temp=temp.next
 q=Queue(4)
 q.enqueue(5)
 q.enqueue(6)
 q.enqueue(7)
+q.dequeue()
+q.dequeue()
+q.enqueue(9)
 q.dequeue()
 q.dequeue()
 q.print_queue()
