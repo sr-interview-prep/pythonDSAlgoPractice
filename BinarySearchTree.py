aa9456921d108729e8d53935318a3fcbf044adbc
suravichandran <suravichandran@paypal.com>
2022-02-14 15:59:51 +0530

    BST updates

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -3,6 +3,7 @@
 no. of nodes in a Binary Search tree are 2^n-1 where n is the no. of levels
 O(log(n)) to search any element within the tree
 '''
+'insert, contains, min_value_node'
 
 class Node:
     def __init__(self, value):
@@ -12,21 +13,21 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root=None
-    def insert(self,value):
+    def insert(self, value):
         new_node=Node(value)
-        if self.root == None:
+        if self.root==None:
             self.root=new_node
             return True
         temp=self.root
-        while(True):
-            if new_node.value==temp.value:
+        while True:
+            if value==temp.value:
                 return False
-            if new_node.value<temp.value:
+            if value<temp.value:
                 if temp.left is None:
                     temp.left=new_node
                     return True
                 temp=temp.left
-            elif new_node.value>temp.value:
+            if value>temp.value:
                 if temp.right is None:
                     temp.right=new_node
                     return True
@@ -35,25 +36,21 @@ class BinarySearchTree:
         if self.root is None:
             return False
         temp=self.root
-        while(True):
-            if value==temp.value:
+        while True:
+            if temp.value==value:
                 return True
-            if value< temp.value:
-                if temp.left is not None:
-                    temp=temp.left
-                else:
+            if value<temp.value:
+                if temp.left is None:
                     return False
-            elif value>temp.value:
-                if temp.right is not None:
-                    temp=temp.right
-                else:
+                temp=temp.left
+            if value>temp.value:
+                if temp.right is None:
                     return False
+                temp=temp.right
     def min_value_node(self, current_node):
         while current_node.left is not None:
             current_node=current_node.left
         return current_node
-
-
 
 bst=BinarySearchTree()
 bst.insert(47)
