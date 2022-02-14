58f54049fe3b3c42d27159953a35498bd50de822
suravichandran <suravichandran@paypal.com>
2022-02-14 15:36:24 +0530

    graph updates

--- Graphs.py
+++ Graphs.py
@@ -1,28 +1,21 @@
 class Graph:
     def __init__(self):
         self.adj_list={}
-    def print_graph(self):
-        for k,v in self.adj_list.items():
-            print(k ,':', v)
-    def add_vertex(self, vertex):
+    def add_vertex(self,vertex):
         if vertex not in self.adj_list.keys():
             self.adj_list[vertex]=[]
             return True
         return False
-    def add_edge(self, v1,v2):
+    def add_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             self.adj_list[v1].append(v2)
             self.adj_list[v2].append(v1)
-            # yet to write a condition for duplicates
             return True
         return False
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            try:
-                self.adj_list[v1].remove(v2)
-                self.adj_list[v2].remove(v1)
-            except ValueError:
-                pass
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
             return True
         return False
     def remove_vertex(self, vertex):
@@ -32,6 +25,10 @@ class Graph:
             self.adj_list.pop(vertex)
             return True
         return False
+    def print_graph(self):
+        for k, v in self.adj_list.items():
+            print(k,' : ',v)
+
 g=Graph()
 g.add_vertex('1')
 g.add_vertex('2')
