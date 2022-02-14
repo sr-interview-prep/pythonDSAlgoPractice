19e327081a5acc9da3e0982af8c41490aafb1654
suravichandran <suravichandran@paypal.com>
2022-02-14 18:47:13 +0530

    unique elements

--- Leetcode/Arrays/Unique Elements in a list.py
+++ Leetcode/Arrays/Unique Elements in a list.py
@@ -1,13 +1,21 @@
-class Solution:
-    def uniqueElements(nums):
-        if len(nums) == 0:
-            return 0
-        i = 0
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
-        print(nums)        
-        return i + 1
-                
-print(Solution.uniqueElements([1,1,1,2,3]))
+# find the no. of unique elements in a sorted list
+'''
+Algo
+two pointer approach
+i, j
+loop through j and when ith and jth element are not same:
+increment i and replace ith element with jth element
+this to ensure that the next of jth elements don't have dupes
+'''
+def uniqueElements(nums):
+    if len(nums)==0:
+        return 0
+    i=0
+    for j in range(1,len(nums)):
+        if nums[i]!=nums[j]:
+            i+=1
+            nums[i]=nums[j]    
+    print(nums)        
+    return i+1
+print(uniqueElements([1,1,1,2,2,2,3,3,4,4]))
+
