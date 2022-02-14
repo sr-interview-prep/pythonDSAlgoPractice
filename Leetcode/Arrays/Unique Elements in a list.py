902182746a7d082cccf7e3ecfe7c4dbe86e98f62
suravichandran <suravichandran@paypal.com>
2022-02-14 18:42:02 +0530

    unique elements in a list

---
+++ Leetcode/Arrays/Unique Elements in a list.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniqueElements(nums):
+        if len(nums) == 0:
+            return 0
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        print(nums)        
+        return i + 1
+                
+print(Solution.uniqueElements([1,1,1,2,3]))
