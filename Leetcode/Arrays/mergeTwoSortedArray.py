32c7798409eda33317ff6326117bf500751e1e20
suravichandran <suravichandran@paypal.com>
2022-02-17 16:15:27 +0530

    asd

--- Leetcode/Arrays/mergeTwoSortedArray.py
+++ Leetcode/Arrays/mergeTwoSortedArray.py
@@ -1,3 +1,32 @@
+'''You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ 
+ '''
 class Solution(object):
     def merge(nums1, m, nums2, n):
         """
@@ -15,20 +44,19 @@ class Solution(object):
             if nums1[i] > nums2[j]:
                 nums1[index1] = nums1[i]
                 i -= 1
-                index1 -= 1
-            
+                index1 -= 1    
             elif nums2[j] > nums1[i]:
                 nums1[index1] = nums2[j]
                 j -= 1
                 index1 -= 1
-                
+        #When ith and jth elements are equal in value
             else:
                 nums1[index1] = nums1[i]
                 nums1[index1-1] = nums2[j]
                 i -= 1
                 j -= 1
                 index1 -= 2
-                
+        # If most elements of j are less than elements of i, i will go to zero and we come to this while loop
         while j >= 0:
             nums1[index1] = nums2[j]
             j -= 1
