6282cc348f1457e2b2715f890019cec62210a6c4
suravichandran <suravichandran@paypal.com>
2022-02-27 16:12:33 +0530

    nqueens

--- General DS/Backtracking/N-queen's Problem.py
+++ General DS/Backtracking/N-queen's Problem.py
@@ -7,6 +7,15 @@ class QueensProblem:
     def __init__(self,n):
         self.n=n
         self.chess_table=[[None for i in range(n) for j in range(n)]]
+    def solve_n_queens(self):
+        if self.solve(0): 
+            self.print_queens()
+        else:
+            print('There is no soltion for the problem')
+    def solve(self, col_index):
+        if col_index==self.n:
+            return True
+        
     def print(self):
         print(self.chess_table)
 
