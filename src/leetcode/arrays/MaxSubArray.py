c8c228efb9dcaabc1715c00f89990d36e33a3b55
Subramani R <suravichandran@paypal.com>
2024-04-21 22:05:44 +0530

    test for max sub arrays

--- src/leetcode/arrays/MaxSubArray.py
+++ src/leetcode/arrays/MaxSubArray.py
@@ -1,4 +1,5 @@
-'''Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+"""
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 Example 1:
 
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
@@ -8,17 +9,20 @@ Example 2:
 
 Input: nums = [1]
 Output: 1
-'''
-def maxSubArray(nums):
-    if len(nums)==0:
-        return 0
-    max_sum=current_sum=nums[0]
-    for i in nums[1:]:
-        current_sum=max(i, current_sum+i)
-        # print('cs',current_sum)
-        max_sum=max(max_sum,current_sum)
-        # print('ms',max_sum)
-    return max_sum
+"""
 
-print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
-    
+
+class MaxSubArray:
+    def __init__(self, nums):
+        self.nums = nums
+
+    def get_max_sub_array(self):
+        if len(self.nums) == 0:
+            return 0
+        max_sum = current_sum = self.nums[0]
+        for i in self.nums[1:]:
+            current_sum = max(i, current_sum + i)
+            max_sum = max(max_sum, current_sum)
+        return max_sum
+
+# print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
---
+++ tests/leetcode/arrays/test_max_sub_array.py
@@ -0,0 +1,7 @@
+from leetcode.arrays.MaxSubArray import MaxSubArray
+
+
+def test_max_sub_array():
+    max_sub_array = MaxSubArray(nums=[-2, 1, -3, 4, -1, 2, 1, -5, 4])
+    result = max_sub_array.get_max_sub_array()
+    assert result == 6
