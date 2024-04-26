249233fb32a9bcae10204e65ba96af4aeccc61c0
Subramani R <suravichandran@paypal.com>
2024-04-26 09:35:18 +0530

    sorted square matrix

--- src/leetcode/arrays/SquaresSortedArray.py
+++ src/leetcode/arrays/SquaresSortedArray.py
@@ -1,4 +1,4 @@
-'''
+"""
 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 Example 1:
 
@@ -10,22 +10,25 @@ Example 2:
 
 Input: nums = [-7,-3,2,3,11]
 Output: [4,9,9,49,121]
-'''
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        
-        l=0
-        r=len(nums)-1
-        
+"""
+from typing import List
+
+
+class SquaresSortedArray:
+    def execute(self, nums: List[int]) -> List[int]:
+
+        l = 0
+        r = len(nums) - 1
+
         result = []
         while l <= r:
-            leftVal  = nums[l] * nums[l]
+            leftVal = nums[l] * nums[l]
             rightVal = nums[r] * nums[r]
-            if leftVal >= rightVal :
-                result.insert(0,leftVal)
-                l+=1
+            if leftVal >= rightVal:
+                result.insert(0, leftVal)
+                l += 1
             else:
-                result.insert(0,rightVal)
-                r-=1
-                
+                result.insert(0, rightVal)
+                r -= 1
+
         return result
