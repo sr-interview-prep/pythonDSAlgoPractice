ad01d161dd6fbd74562993dd178af8cdb6111fc3
Subramani R <suravichandran@paypal.com>
2024-05-11 23:52:34 +0530

    insertion sort

--- src/data_structures/Sorting/InsertionSort.py
+++ src/data_structures/Sorting/InsertionSort.py
@@ -7,37 +7,53 @@ Where possibility of achieving linear time complexity is more
 Algorithm
 Start from the second element (index 1) of the list.
 Pick the current element and store it in a temporary variable (temp).
-Compare with previous elements: Move backwards through the sorted portion of the list (from the current position to the beginning)
-Shifting each element to the right until you find the correct position for the current element.
-Insert the element: Once you find the correct position (either because you reached the beginning of the list or you found an element smaller than the current one), insert the temporary variable (temp) into that position.
-Repeat: Continue this process for each element in the list, expanding the sorted portion with each iteration.
-Return the sorted list.
+Compare current element to each of the left side elements of the list
+    - If  current_element<left_element:
+        left_element is moved to its right
+        left_element=current_element
+    - else: 
+        break the loop
+        this means
+            current element is inserted in its right place given the sorted array on the left 
+            left portion of the current element is completely sorted
+At the end of 1st iteration:
+    - The list left to the current element's initial position is sorted
+    - And the way we do it is by making sure     
 '''
 
 
+# current_element:  2
 # [4, 2, 6, 5, 1, 3]
 # [2, 4, 6, 5, 1, 3]
+# current_element:  6
 # [2, 4, 6, 5, 1, 3]
+# current_element:  5
 # [2, 4, 6, 5, 1, 3]
 # [2, 4, 5, 6, 1, 3]
+# current_element:  1
 # [2, 4, 5, 6, 1, 3]
 # [2, 4, 5, 1, 6, 3]
 # [2, 4, 1, 5, 6, 3]
 # [2, 1, 4, 5, 6, 3]
 # [1, 2, 4, 5, 6, 3]
+# current_element:  3
 # [1, 2, 4, 5, 6, 3]
 # [1, 2, 4, 5, 3, 6]
 # [1, 2, 4, 3, 5, 6]
 # [1, 2, 3, 4, 5, 6]
 
+
 def insertion_sort(my_list):
+    print(my_list)
     for i in range(1, len(my_list)):
-        temp = my_list[i]
+        current_element = my_list[i]
+        print("current_element: ", current_element)
+
         j = i - 1
-        while temp < my_list[j] and j > -1:
+        while my_list[j] > current_element and j >= 0:
             print(my_list)
             my_list[j + 1] = my_list[j]
-            my_list[j] = temp
+            my_list[j] = current_element
             j -= 1
         print(my_list)
     return my_list
