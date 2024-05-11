84cc071835f143a4145976f41b8d737088335f7d
Subramani R <suravichandran@paypal.com>
2024-05-11 16:08:17 +0530

    adding ascending and descending methoids

--- src/data_structures/Sorting/BubbleSort.py
+++ src/data_structures/Sorting/BubbleSort.py
@@ -1,6 +1,6 @@
 """
 Algorithm
-Start at the end of the list and look at each pair of neighboring numbers.
+Start with the first element of the list and look at each pair of neighboring numbers.
 Compare each pair: If the first number is bigger than the second, swap them.
 Keep doing this: Move through the list, comparing and swapping pairs, until you've gone through the whole list once.
 Repeat: Go through the list again, but this time stop before the last element because the biggest number is already in its place.
@@ -11,11 +11,24 @@ Finished: Once no more swaps are needed, the list is sorted, and you're done!
 from typing import List
 
 
-def bubble_sort(my_list: List):
-    for i in range(len(my_list) - 1, 0, -1):
-        for j in range(i):  # first time whole list, 2nd time n-1, n-2. just the first number
-            if my_list[j] > my_list[j + 1]:
-                temp = my_list[j]
-                my_list[j] = my_list[j + 1]
-                my_list[j + 1] = temp
-    return my_list
+class BubbleSort:
+
+    @staticmethod
+    def ascending(input_list: List):
+        for i in range(len(input_list) - 1, 0, -1):
+            for j in range(i):
+                if input_list[j] > input_list[j + 1]:
+                    temp = input_list[j]
+                    input_list[j] = input_list[j + 1]
+                    input_list[j + 1] = temp
+        return input_list
+
+    @staticmethod
+    def descending(input_list: List):
+        for i in range(len(input_list) - 1, 0, -1):
+            for j in range(i):
+                if input_list[j] < input_list[j + 1]:
+                    temp = input_list[j]
+                    input_list[j] = input_list[j + 1]
+                    input_list[j + 1] = temp
+        return input_list
