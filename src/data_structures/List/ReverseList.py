0e4cc7ded27cf938748ee960b71e2a45484d4a39
Subramani R <subramani0595@gmail.com>
2024-06-18 00:09:06 +0530

    reverse lists

--- src/data_structures/List/ReverseList.py
+++ src/data_structures/List/ReverseList.py
@@ -5,9 +5,20 @@ class ReverseList:
     def built_in_method(self):
         return self.input_list[::-1]
 
-    def linear_method(self):
+    def linear_method_basic(self):
         # Time complexity: O(N)
+        # Space complexity: O(N)
         result = []
         for i in range(len(self.input_list) - 1, -1, -1):
             result.append(self.input_list[i])
         return result
+
+    def linear_method_inplace(self):
+        # Time complexity: O(N/2)
+        # Space complexity: O(1)
+        n = len(self.input_list)
+        for i in range(n // 2):
+            temp = self.input_list[i]
+            self.input_list[i] = self.input_list[n - i - 1]
+            self.input_list[n - i - 1] = temp
+        return self.input_list
--- tests/data_structures/List/test_reverse_list.py
+++ tests/data_structures/List/test_reverse_list.py
@@ -1,11 +1,21 @@
 from data_structures.List.ReverseList import ReverseList
 
 
-def test_reverse_list():
+def test_reverse_list_basic():
     reverse_list = ReverseList(input_list=[3, 4, 5, 6])
     result = reverse_list.built_in_method()
     assert result == [6, 5, 4, 3]
 
     reverse_list = ReverseList(input_list=[3, 4, 5, 6])
-    result = reverse_list.linear_method()
+    result = reverse_list.linear_method_basic()
     assert result == [6, 5, 4, 3]
+
+
+def test_reverse_list_inplace():
+    reverse_list = ReverseList(input_list=[3, 4, 5, 6])
+    result = reverse_list.linear_method_inplace()
+    assert result == [6, 5, 4, 3]
+
+    reverse_list = ReverseList(input_list=[3, 4, 5, 6, 7])
+    result = reverse_list.linear_method_inplace()
+    assert result == [7, 6, 5, 4, 3]
