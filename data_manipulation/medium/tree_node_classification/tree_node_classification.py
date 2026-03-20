177287f3ef3e467252c13bc8101a700a1a0c8278
Subramani Ravichandran <sravichandran3@atlassian.com>
2026-03-20 18:29:50 +0530

    fixing pyspark

--- data_manipulation/medium/tree_node_classification/tree_node_classification.py
+++ data_manipulation/medium/tree_node_classification/tree_node_classification.py
@@ -5,13 +5,22 @@ def tree_node_classification(Tree: DataFrame) -> DataFrame:
     t = Tree.alias("t")
     c = Tree.alias("c")
 
-    joined_df=t.join(c, F.col("t.id") == F.col("c.p_id"), "left")
-    grouped_df=joined_df.groupBy(F.col("t.id"), F.col("t.p_id")).agg(F.count(F.col("c.id")).alias("child_count"))
-    result_df=grouped_df.select(
-        F.col("t.id"),
+    
+    joined_df = t.join(c, F.col("t.id") == F.col("c.p_id"), "left")
+
+    
+    grouped_df = joined_df.groupBy(F.col("t.id"), F.col("t.p_id")).agg(
+        F.count(F.col("c.id")).alias("child_count")
+    )
+
+    
+    result_df = grouped_df.select(
+        F.col("t.id").alias("id"),
         F.when(F.col("t.p_id").isNull(), F.lit("Root"))
         .when(F.col("child_count") == 0, F.lit("Leaf"))
         .otherwise(F.lit("Inner"))
         .alias("type"),
     )
-    return result_df.orderBy(F.col("t.id"))
+
+    
+    return result_df.orderBy(F.col("id"))
