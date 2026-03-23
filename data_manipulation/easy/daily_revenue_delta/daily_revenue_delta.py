5b84c5dd60d3776e0dca2cb15cc741943d019118
Subramani Ravichandran <sravichandran3@atlassian.com>
2026-03-23 16:53:23 +0530

    fixing syntax

--- data_manipulation/easy/daily_revenue_delta/daily_revenue_delta.py
+++ data_manipulation/easy/daily_revenue_delta/daily_revenue_delta.py
@@ -3,11 +3,17 @@ from pyspark.sql import functions as F
 
 
 def daily_revenue_delta(daily_sales: DataFrame) -> DataFrame:
+    
+    window_spec = Window.orderBy(F.col("sale_date"))
+    result = daily_sales \
+            .select(
+                F.col("sale_date"),
+                F.col("revenue"),
+                (F.col("revenue") - F.lag(F.col("revenue")).over(window_spec))
+                    .alias("revenue_delta")
+            ) \
+            .orderBy("sale_date") \
+            .limit(3)
 
-    result_df = daily_sales.select(
-        F.col("sale_date"),
-        F.col("revenue"),
-        (F.col("revenue") - F.lag(F.col("revenue")).over(Window.orderBy(F.col("sale_date")))).alias("revenue_delta"),
-    ).orderBy(F.col("sale_date")).limit(3)
-
-    return result_df
+    return result
+    
