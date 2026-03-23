55af7459e4829d77d8f6c2e1bd0ebb364d6f8e74
Subramani Ravichandran <sravichandran3@atlassian.com>
2026-03-23 15:59:54 +0530

    updating the results

--- data_manipulation/easy/actors_atleast_cooperated_3_times/actors_atleast_cooperated_3_times.py
+++ data_manipulation/easy/actors_atleast_cooperated_3_times/actors_atleast_cooperated_3_times.py
@@ -4,12 +4,10 @@ from pyspark.sql import DataFrame, functions as F
 def actors_atleast_cooperated_3_times(actordirector: DataFrame) -> DataFrame:
 
     
-    result = actordirector.groupby('actor_id','director_id') \
-    .agg(F.count(F.lit(1)).alias("cooperation_count")) \
-    .filter(F.col("cooperation_count")>=3) \
-    .select("actor_id","director_id")
-
+    result = actordirector.groupBy("actor_id","director_id") \
+            .agg(F.count(F.lit(1)).alias("cooperated_count") ) \
+            .filter(F.col("cooperated_count")>=3)\
+            .select("actor_id","director_id")
     return result
-
     
     
