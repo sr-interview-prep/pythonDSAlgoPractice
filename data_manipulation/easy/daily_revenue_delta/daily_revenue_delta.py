d2c200423a4f75bb773a924a680436925b8a7794
Subramani Ravichandran <sravichandran3@atlassian.com>
2026-03-23 17:28:19 +0530

    daily revenue delta

--- data_manipulation/easy/daily_revenue_delta/daily_revenue_delta.py
+++ data_manipulation/easy/daily_revenue_delta/daily_revenue_delta.py
@@ -4,16 +4,12 @@ from pyspark.sql import functions as F
 
 def daily_revenue_delta(daily_sales: DataFrame) -> DataFrame:
     
-    window_spec = Window.orderBy(F.col("sale_date"))
-    result = daily_sales \
-            .select(
-                F.col("sale_date"),
-                F.col("revenue"),
-                (F.col("revenue") - F.lag(F.col("revenue")).over(window_spec))
-                    .alias("revenue_delta")
-            ) \
-            .orderBy("sale_date") \
-            .limit(3)
-
-    return result
+    window_spec=Window.orderBy("sale_date")
     
+    return daily_sales.select(
+        F.col("sale_date"),
+        F.col("revenue"),
+        (F.col("revenue")-F.lag(F.col("revenue")).over(window_spec)).alias("revenue_delta")
+        )\
+        .orderBy(F.col("sale_date"))\
+        .limit(3)
