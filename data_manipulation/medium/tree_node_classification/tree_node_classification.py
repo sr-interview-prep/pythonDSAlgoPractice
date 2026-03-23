61d0254d34445a92f628d7faf8b2d99cc2903756
Subramani Ravichandran <sravichandran3@atlassian.com>
2026-03-23 17:49:56 +0530

    tree node pyspark

--- data_manipulation/medium/tree_node_classification/tree_node_classification.py
+++ data_manipulation/medium/tree_node_classification/tree_node_classification.py
@@ -2,25 +2,33 @@ from pyspark.sql import DataFrame, functions as F
 
 
 def tree_node_classification(Tree: DataFrame) -> DataFrame:
-    t = Tree.alias("t")
-    c = Tree.alias("c")
+    t=Tree.alias("t")
+    c=Tree.alias("c")
 
+    joined_df=t.join(
+                c, 
+                F.col("t.id")==F.col("c.p_id"),
+                "left"
+            ).groupBy(
+                F.col("t.id"),
+                F.col("t.p_id")
+            ).agg(
+                F.count(F.col("c.id")).alias("count_children")
+            )\
+            .select(
+                F.col("id"),
+                F.when(
+                        F.col("p_id").isNull(),
+                        F.lit("Root")
+                    ).when(
+                        F.col("count_children") == 0,
+                        F.lit("Leaf")
+                    ).otherwise(
+                        F.lit("Inner")
+                ).alias("type")
+                )\
+            .orderBy(
+                F.col("id")
+            )
+    return joined_df
     
-    joined_df = t.join(c, F.col("t.id") == F.col("c.p_id"), "left")
-
-    
-    grouped_df = joined_df.groupBy(F.col("t.id"), F.col("t.p_id")).agg(
-        F.count(F.col("c.id")).alias("child_count")
-    )
-
-    
-    result_df = grouped_df.select(
-        F.col("t.id").alias("id"),
-        F.when(F.col("t.p_id").isNull(), F.lit("Root"))
-        .when(F.col("child_count") == 0, F.lit("Leaf"))
-        .otherwise(F.lit("Inner"))
-        .alias("type"),
-    )
-
-    
-    return result_df.orderBy(F.col("id"))
