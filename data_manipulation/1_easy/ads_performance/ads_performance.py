73d48c19728d1e54ce8442154dc46521c4a1dc46
Subramani Ravichandran <sravichandran3@atlassian.com>
2026-03-23 18:17:57 +0530

    ads performance

--- data_manipulation/1_easy/ads_performance/ads_performance.py
+++ data_manipulation/1_easy/ads_performance/ads_performance.py
@@ -3,25 +3,24 @@ from pyspark.sql import DataFrame, functions as F
 
 def ads_performance(ads: DataFrame) -> DataFrame:
 
-    result = ads.groupBy("ad_id")\
-        .agg(
-            F.sum(
-                F.when(F.col("action")=="Clicked",1) \
-                .otherwise(0)
-                ).alias("clicked_count"), 
-            F.sum(
-                F.when(F.col("action") \
-                .isin("Clicked", "Viewed"),1) \
-                .otherwise(0)
-                ).alias("total")
-            ) \
-        .withColumn(
-            "ctr",
-                F.when(F.col("total") == 0, F.lit(0.0)) \
-                .otherwise(
-                    F.round(
-                        (F.col("clicked_count") / F.col("total")) * 100,
-                    2))) \
-        .select("ad_id", "ctr")
+# sum of clicked
+# sum of clicked and viewed
+# ratio for cte
 
-    return result
+    return ads.groupBy(
+            F.col("ad_id")
+        ).agg(
+            F.sum(F.when(F.col("action")=="Clicked",1).otherwise(0)).alias("sum_clicked"),
+            F.sum(F.when(F.col("action").isin("Clicked","Viewed"),1).otherwise(0)).alias("sum_viewed_clicked")
+            ) \
+        .select(
+            F.col("ad_id"),
+            F.round(
+                F.when(F.col("sum_viewed_clicked")==0,0) \
+                .otherwise(F.col("sum_clicked")*100/F.col("sum_viewed_clicked")),
+                2
+                ).alias("ctr")) \
+        .orderBy(
+            F.col("ctr").desc(),
+            F.col("ad_id")
+        )
